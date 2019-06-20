72f6139aa839d0b85fbb2dd0479cfe490a267fcc
J0eppp <joep.van.dijk04@gmail.com>
2019-06-20 15:38:30 +0200

    Update

--- docs/dot/dot.js
+++ docs/dot/dot.js
@@ -7,13 +7,15 @@ var config = {
     {x: 0, y: 150},
     {x: 250, y: 200},
     {x: 300, y: 200},
+    {x: 400, y: 200},
     {x: 500, y: 400},
     {x: 600, y: 300}
-  ], settings: {
-    resize: true
+  ],
+  settings: {
+    resize: true,
+    lineWidth: 2,
+    dotSize: 8
   }
 }
 
-
 var chart = new Charty(ctx, config);
-console.log(chart);
